9904042caf82ea46adbec26768fdf8b411ad3ae3
Althafrizkys <althafrizkys@gmail.com>
2026-09-21 22:56:28 +0700

    feat: selesaikan tugas modul 6 model eloquent dan relasi

---
+++ app/Models/Product.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
+class Product extends Model
+{
+    protected $fillable = ['category_id', 'code', 'name', 'unit', 'price', 'stock'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function transactionDetails()
+    {
+        return $this->hasMany(TransactionDetail::class);
+    }
+
+    //ACCESSOR
+    protected function formattedPrice(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => 'Rp ' . number_format($this->price, 0, ',', '.')
+        );
+    }
+}
